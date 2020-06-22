134925093cb04fd66bbf10e3dd9ebe004ebf5270
Dallas Lu <i@luda.me>
2020-06-22 20:01:05 +0800

    Update bootstrap5-dropdown-ml-hack.js

--- bootstrap5-dropdown-ml-hack.js
+++ bootstrap5-dropdown-ml-hack.js
@@ -4,16 +4,16 @@
             document.querySelectorAll('.has-child-dropdown-show').forEach(function(e) {
                 e.classList.remove('has-child-dropdown-show');
             });
-            let dd = this._element.closest('.dropdown').closest('.dropdown');
-            for (; dd && dd !== document; dd = dd.closest('.dropdown')) {
+            let dd = this._element.closest('.dropdown').parentNode.closest('.dropdown');
+            for (; dd && dd !== document; dd = dd.parentNode.closest('.dropdown')) {
                 dd.classList.add('has-child-dropdown-show');
             }
             return _orginal.call(this);
         }
     }($bs.Dropdown.prototype.toggle);
+
     document.querySelectorAll('.dropdown').forEach(function(dd) {
         dd.addEventListener('hide.bs.dropdown', function(e) {
-            console.log(e);
             if (this.classList.contains('has-child-dropdown-show')) {
                 this.classList.remove('has-child-dropdown-show');
                 e.preventDefault();
