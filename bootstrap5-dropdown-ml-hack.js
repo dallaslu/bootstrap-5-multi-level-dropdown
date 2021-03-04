f9799551e1b8b8038c282bdc9066d4ad62f6918a
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2021-03-04 15:44:31 +0800

    update to bootstrap 5.0.0-beta2

--- bootstrap5-dropdown-ml-hack.js
+++ bootstrap5-dropdown-ml-hack.js
@@ -1,12 +1,13 @@
 (function($bs) {
+	const CLASS_NAME = 'has-child-dropdown-show';
     $bs.Dropdown.prototype.toggle = function(_orginal) {
         return function() {
-            document.querySelectorAll('.has-child-dropdown-show').forEach(function(e) {
-                e.classList.remove('has-child-dropdown-show');
+            document.querySelectorAll('.' + CLASS_NAME).forEach(function(e) {
+                e.classList.remove(CLASS_NAME);
             });
             let dd = this._element.closest('.dropdown').parentNode.closest('.dropdown');
             for (; dd && dd !== document; dd = dd.parentNode.closest('.dropdown')) {
-                dd.classList.add('has-child-dropdown-show');
+                dd.classList.add(CLASS_NAME);
             }
             return _orginal.call(this);
         }
@@ -14,8 +15,8 @@
 
     document.querySelectorAll('.dropdown').forEach(function(dd) {
         dd.addEventListener('hide.bs.dropdown', function(e) {
-            if (this.classList.contains('has-child-dropdown-show')) {
-                this.classList.remove('has-child-dropdown-show');
+            if (this.classList.contains(CLASS_NAME)) {
+                this.classList.remove(CLASS_NAME);
                 e.preventDefault();
             }
             e.stopPropagation(); // do not need pop in multi level mode
@@ -29,13 +30,13 @@
 
     document.querySelectorAll('.dropdown-hover, .dropdown-hover-all .dropdown').forEach(function(dd) {
         dd.addEventListener('mouseenter', function(e) {
-            let toggle = e.target.querySelector(':scope>[data-toggle="dropdown"]');
+            let toggle = e.target.querySelector(':scope>[data-bs-toggle="dropdown"]');
             if (!toggle.classList.contains('show')) {
                 getDropdown(toggle).toggle();
             }
         });
         dd.addEventListener('mouseleave', function(e) {
-            let toggle = e.target.querySelector(':scope>[data-toggle="dropdown"]');
+            let toggle = e.target.querySelector(':scope>[data-bs-toggle="dropdown"]');
             if (toggle.classList.contains('show')) {
                 getDropdown(toggle).toggle();
             }
